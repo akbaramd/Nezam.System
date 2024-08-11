1e14d32035eccc4d73e170bbbe351c754780cbca
ahmadi <akbarsafari00@gmail.com>
2024-08-11 17:44:54 +0330

    .

---
+++ Nezam.Web.App/Controllers/ReportController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Velzon.Controllers;
+
+public class ReportController : Controller
+{
+  // GET
+  public IActionResult Index()
+  {
+    
+    return View();
+  }
+}
