535a01050bc593f8ee2a4046fdb54d069a636c05
ahmadi <akbarsafari00@gmail.com>
2025-03-11 12:32:45 +0330

    1

---
+++ Nezam.Web.App/Migrations/20250310135846_Init3.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Nezam.System.Web.Migrations
+{
+    /// <inheritdoc />
+    public partial class Init3 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "UserAgencies",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "TEXT", nullable: false),
+                    AgencyCode = table.Column<int>(type: "INTEGER", nullable: false),
+                    UserId = table.Column<Guid>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserAgencies", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_UserAgencies_AspNetUsers_UserId",
+                        column: x => x.UserId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserAgencies_UserId",
+                table: "UserAgencies",
+                column: "UserId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "UserAgencies");
+        }
+    }
+}
